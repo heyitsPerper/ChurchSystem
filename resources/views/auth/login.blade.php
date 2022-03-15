6202905b7f7b1711839b42fe597cddcf35ed4340
heyitsperper <ruizferlyn123@gmail.com>
2022-03-15 20:05:34 +0800

    First Commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,8 +11,12 @@
                 <div class="row">
                     <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
                     <div class="col-lg-6">
+                    <a onclick="return confirm('Are you sure to leave this page?')" href="/" class="float-right btn-close btn-sm" aria-label="Close">X</a>
+                        
                         <div class="p-5">
                             <div class="text-center">
+                            
+
                                 <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                             </div>
                             <form method="POST" action="{{ route('login') }}">
